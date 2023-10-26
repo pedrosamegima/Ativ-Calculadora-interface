8eb8f03720cd785db50fa26a0704895ef01120cd
pedrosamegima <136722484+pedrosamegima@users.noreply.github.com>
2023-10-26 16:04:43 -0300

    Add files via upload

---
+++ Ativ2/Principal.java
@@ -0,0 +1,20 @@
+package Ativ2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Calculando calculando = new Calculando();
+		
+		System.out.println(calculando.somar(8, 7));
+		
+		System.out.println(calculando.sub(18, 6));
+		
+		System.out.println(calculando.mult(8, 5));
+		
+		System.out.println(calculando.div(50, 10));
+		
+		System.out.println(calculando.exp(8, 2));
+
+	}
+
+}
